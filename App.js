285ae718f26b1639efb948542df872d6fec4628b
tkgstrator <nasawake.am@gmail.com>
2020-06-25 18:09:54 -0400

    Add WebView.

--- App.js
+++ App.js
@@ -1,52 +1,86 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { Text, View, Alert, Button, WebView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+// import { Button } from "native-base";
 
-function HomeScreen() {
+class SalmonStats extends Component {
+  render() {
+    return <WebView source={{ uri: "https://logrocket.com/" }} />;
+  }
+}
+
+function Profile() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
+      <Text>Profile!</Text>
     </View>
   );
 }
 
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
-    </View>
-  );
+class Config extends Component {
+  loginSplatNet2 = () => {
+    Alert.alert("Login!");
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Button title="SplatNet2" onPress={this.loginSplatNet2} />
+        {/* <Text>Notifications!</Text> */}
+      </View>
+    );
+  }
 }
 
 const Tab = createBottomTabNavigator();
 
-export default function App() {
+function MyTabs() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator
+      initialRouteName="Feed"
+      tabBarOptions={{
+        activeTintColor: "#e91e63",
+      }}
+    >
+      <Tab.Screen
+        name="SalmonStats"
+        component={SalmonStats}
+        options={{
+          tabBarLabel: "SalmonStats",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Config"
+        component={Config}
+        options={{
+          tabBarLabel: "Config",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Welcome to Salmonia</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
