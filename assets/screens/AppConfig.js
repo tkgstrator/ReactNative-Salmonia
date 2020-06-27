42f4b18a94a642452baba2e5159fd6d1a6702799
tkgstrator <nasawake.am@gmail.com>
2020-06-27 19:03:53 -0400

    Apply getting iksm_session by DeepLink.

--- assets/screens/AppConfig.js
+++ assets/screens/AppConfig.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { TextInput, View, Button, Linking, StyleSheet } from "react-native";
+import { TextInput, View, Button, Linking, StyleSheet, AsyncStorage } from "react-native";
 import { Body } from "native-base";
 
 async function getOAuthURL() {
@@ -20,12 +20,29 @@ class AppConfig extends Component {
     Linking.openURL(oauth["auth_url"]);
   };
 
+  getResults = async () => {
+    let iksm_session = await AsyncStorage.getItem('@iksm_session:key');
+    let url, header, response
+
+    console.log(iksm_session)
+    url = "https://app.splatoon2.nintendo.net/api/coop_results"
+    header = {
+      "cookie": "iksm_session=" + iksm_session,
+    }
+    response = await fetch(url, {
+      method: "GET",
+      headers: header,
+    });
+    let json = await response.json()
+    let job_num = json["summary"]["card"]["job_num"]
+    console.log(job_num)
+  }
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Button title="SplatNet2" onPress={this.loginSplatNet2} />
-        {/* <Button title="DeepLink Test" onPress={() => Linking.openURL("npf71b963c1b7b6d119://auth")} /> */}
-        {/* <Button title="Generate URL" onPress={this.loginSplatNet2} /> */}
+        <Button title="Download Result from SplatNet2" onPress={this.getResults} />
       </View>
     );
   }
